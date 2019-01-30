26ad528c11f1f87a741edbe3ca6e3387f34183b3
Markku Uusitupa <markku@richie.fi>
2019-01-30 12:06:30 +0200

    Make admin settings menu

--- richie-news-feed/admin/class-richie-news-feed-admin.php
+++ richie-news-feed/admin/class-richie-news-feed-admin.php
@@ -1,103 +1,144 @@
 <?php
 
 /**
- * The admin-specific functionality of the plugin.
- *
- * @link       https://www.richie.fi
- * @since      1.0.0
- *
- * @package    Richie_News_Feed
- * @subpackage Richie_News_Feed/admin
- */
+* The admin-specific functionality of the plugin.
+*
+* @link       https://www.richie.fi
+* @since      1.0.0
+*
+* @package    Richie_News_Feed
+* @subpackage Richie_News_Feed/admin
+*/
 
 /**
- * The admin-specific functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
- *
- * @package    Richie_News_Feed
- * @subpackage Richie_News_Feed/admin
- * @author     Markku Uusitupa <markku@richie.fi>
- */
+* The admin-specific functionality of the plugin.
+*
+* Defines the plugin name, version, and two examples hooks for how to
+* enqueue the admin-specific stylesheet and JavaScript.
+*
+* @package    Richie_News_Feed
+* @subpackage Richie_News_Feed/admin
+* @author     Markku Uusitupa <markku@richie.fi>
+*/
 class Richie_News_Feed_Admin {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+    * The ID of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $plugin_name    The ID of this plugin.
+    */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+    * The version of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $version    The current version of this plugin.
+    */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
+    /**
+    * Initialize the class and set its properties.
+    *
+    * @since    1.0.0
+    * @param      string    $plugin_name       The name of this plugin.
+    * @param      string    $version    The version of this plugin.
+    */
+    public function __construct( $plugin_name, $version ) {
 
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
 
-	}
+    }
 
-	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+    /**
+    * Register the stylesheets for the admin area.
+    *
+    * @since    1.0.0
+    */
+    public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Richie_News_Feed_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Richie_News_Feed_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+        * This function is provided for demonstration purposes only.
+        *
+        * An instance of this class should be passed to the run() function
+        * defined in Richie_News_Feed_Loader as all of the hooks are defined
+        * in that particular class.
+        *
+        * The Richie_News_Feed_Loader will then create the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/richie-news-feed-admin.css', array(), $this->version, 'all' );
+        wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/richie-news-feed-admin.css', array(), $this->version, 'all' );
 
-	}
+    }
 
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+    /**
+    * Register the JavaScript for the admin area.
+    *
+    * @since    1.0.0
+    */
+    public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Richie_News_Feed_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Richie_News_Feed_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+        * This function is provided for demonstration purposes only.
+        *
+        * An instance of this class should be passed to the run() function
+        * defined in Richie_News_Feed_Loader as all of the hooks are defined
+        * in that particular class.
+        *
+        * The Richie_News_Feed_Loader will then create the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/richie-news-feed-admin.js', array( 'jquery' ), $this->version, false );
+        wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/richie-news-feed-admin.js', array( 'jquery' ), $this->version, false );
 
-	}
+    }
+
+    /**
+    * Register the administration menu for this plugin into the WordPress Dashboard menu.
+    *
+    * @since    1.0.0
+    */
+
+    public function add_plugin_admin_menu() {
+
+        /*
+        * Add a settings page for this plugin to the Settings menu.
+        *
+        * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+        *
+        *        Administration Menus: http://codex.wordpress.org/Administration_Menus
+        *
+        */
+        add_options_page( 'Richie News Settings', 'Richie News', 'manage_options', $this->plugin_name, array($this, 'load_admin_page_content') );
+    }
+
+    /**
+    * Add settings action link to the plugins page.
+    *
+    * @since    1.0.0
+    */
+
+    public function add_action_links( $links ) {
+        /*
+        *  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+        */
+        $settings_link = array(
+            '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+        );
+        return array_merge(  $settings_link, $links );
+
+    }
+
+    // Load the plugin admin page partial.
+    public function load_admin_page_content() {
+        require_once plugin_dir_path( __FILE__ ). 'partials/richie-news-feed-admin-display.php';
+    }
 
 }
