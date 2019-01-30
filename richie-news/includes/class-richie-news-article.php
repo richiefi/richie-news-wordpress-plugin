3b4c291a811b922d17dc342901d0fc835b51bc57
Markku Uusitupa <markku@richie.fi>
2019-01-30 15:48:44 +0200

    Use options for mapping metered_paywall values to pmpro levels.

--- richie-news/includes/class-richie-news-article.php
+++ richie-news/includes/class-richie-news-article.php
@@ -1,36 +1,47 @@
 <?php
 
 class Richie_News_Article {
-  public $title;
-  public $content_html_document;
-  public $date;
-  public $metered_paywall;
-  public $updated_date;
 
-  function __construct($post) {
+    // values for metered_paywall
+    const METERED_PAYWALL_NO_ACCESS_VALUE = 'no_access';
+    const METERED_PAYWALL_METERED_VALUE = 'metered';
+    const METERED_PAYWALL_FREE_VALUE = 'free';
 
-    global $wpdb;
+    public $title;
+    public $content_html_document;
+    public $date;
+    public $metered_paywall;
+    public $updated_date;
 
-    $content = $post->post_content;
-    $content = apply_filters('the_content', $content);
-    $content = str_replace(']]>', ']]&gt;', $content);
-    $sqlQuery = "SELECT m.id, m.name FROM $wpdb->pmpro_memberships_pages mp LEFT JOIN $wpdb->pmpro_membership_levels m ON mp.membership_id = m.id WHERE mp.page_id = '" . $post->ID . "'";
-    $post_membership_levels = $wpdb->get_results($sqlQuery);
-    $level_names= array_column($post_membership_levels, 'name');
-    $is_premium = in_array('Premium', $level_names);
-    $is_metered = in_array('Metered', $level_names);
-    if ( $is_premium ) {
-      $this->metered_paywall = 'no_access';
-    } else if ( $is_metered  ) {
-      $this->metered_paywall = 'metered';
-    } else {
-      $this->metered_paywall = 'free';
+    function __construct($post, $richie_news_options) {
+
+        global $wpdb;
+
+        $metered_id = $richie_news_options['metered_pmpro_level'];
+        $member_only_id = $richie_news_options['member_only_pmpro_level'];
+
+        $content = $post->post_content;
+        $content = apply_filters('the_content', $content);
+        $content = str_replace(']]>', ']]&gt;', $content);
+        $sqlQuery = "SELECT m.id, m.name FROM $wpdb->pmpro_memberships_pages mp LEFT JOIN $wpdb->pmpro_membership_levels m ON mp.membership_id = m.id WHERE mp.page_id = '" . $post->ID . "'";
+        $post_membership_levels = $wpdb->get_results($sqlQuery);
+        $levels = array_column($post_membership_levels, 'id');
+
+        $is_premium = in_array($member_only_id, $levels);
+        $is_metered = in_array($metered_id, $levels);
+
+        if ( $is_premium ) {
+            $this->metered_paywall = self::METERED_PAYWALL_NO_ACCESS_VALUE;
+        } else if ( $is_metered  ) {
+            $this->metered_paywall = self::METERED_PAYWALL_METERED_VALUE;
+        } else {
+            $this->metered_paywall = self::METERED_PAYWALL_FREE_VALUE;
+        }
+
+        $this->title = $post->post_title;
+        $this->content_html_document = $content;
+        $this->date = (new DateTime($post->post_date_gmt))->format('c');
+        $this->updated_date = (new DateTime($post->post_modified_gmt))->format('c');
+
     }
-
-    $this->title = $post->post_title;
-    $this->content_html_document = $content;
-    $this->date = (new DateTime($post->post_date_gmt))->format('c');
-    $this->updated_date = (new DateTime($post->post_modified_gmt))->format('c');
-
-  }
 }
--- richie-news/public/class-richie-news-public.php
+++ richie-news/public/class-richie-news-public.php
@@ -1,134 +1,146 @@
 <?php
 
 /**
- * The public-facing functionality of the plugin.
- *
- * @link       https://www.richie.fi
- * @since      1.0.0
- *
- * @package    Richie_News
- * @subpackage Richie_News/public
- */
+* The public-facing functionality of the plugin.
+*
+* @link       https://www.richie.fi
+* @since      1.0.0
+*
+* @package    Richie_News
+* @subpackage Richie_News/public
+*/
 
 
 /**
- * The public-facing functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the public-facing stylesheet and JavaScript.
- *
- * @package    Richie_News
- * @subpackage Richie_News/public
- * @author     Markku Uusitupa <markku@richie.fi>
- */
+* The public-facing functionality of the plugin.
+*
+* Defines the plugin name, version, and two examples hooks for how to
+* enqueue the public-facing stylesheet and JavaScript.
+*
+* @package    Richie_News
+* @subpackage Richie_News/public
+* @author     Markku Uusitupa <markku@richie.fi>
+*/
 class Richie_News_Public {
 
 	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
+    * The ID of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $plugin_name    The ID of this plugin.
+    */
 	private $plugin_name;
 
 	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    * The version of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $version    The current version of this plugin.
+    */
+    private $version;
+
+    /**
+     * Richie News options
+     *
+     * @since   1.0.0
+     * @access  private
+     * @var     array      $richie_news_options
+     */
+
+    private $richie_news_options;
 
 	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
+    * Initialize the class and set its properties.
+    *
+    * @since    1.0.0
+    * @param      string    $plugin_name       The name of the plugin.
+    * @param      string    $version    The version of this plugin.
+    */
 	public function __construct( $plugin_name, $version ) {
-    require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-richie-news-article.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-richie-news-article.php';
 		$this->plugin_name = $plugin_name;
-    $this->version = $version;
-  }
+        $this->version = $version;
+        $this->richie_news_options = get_option($plugin_name);
 
-  function feed_route_handler($data) {
-    $args = array(
-      'numberposts' => 10,
-      'suppress_filters' => false
-    );
-    $posts = get_posts($args);
-    $articles = array();
-    foreach ($posts as $content_post) {
-      $post = get_post($content_post);
-      $article = new Richie_News_Article($post);
-      array_push($articles, $article);
     }
 
-    return array('articles' => $articles);
-  }
+    function feed_route_handler($data) {
+        $args = array(
+            'numberposts' => 10,
+            'suppress_filters' => false
+        );
+        $posts = get_posts($args);
+        $articles = array();
+        foreach ($posts as $content_post) {
+            $post = get_post($content_post);
+            $article = new Richie_News_Article($post, $this->richie_news_options);
+            array_push($articles, $article);
+        }
 
-  /**
-	 * Register rest api route for richie feed
-	 *
-	 * @since    1.0.0
-	 */
-  public function register_richie_rest_api() {
-    apply_filters("pmpro_has_membership_access_filter", true);
-    register_rest_route( 'richie/v1', '/news', array(
-      'methods' => 'GET',
-      'callback' => array($this, 'feed_route_handler'),
-    ) );
-  }
+        return array('articles' => $articles);
+    }
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+    /**
+    * Register rest api route for richie feed
+    *
+    * @since    1.0.0
+    */
+    public function register_richie_rest_api() {
+        apply_filters("pmpro_has_membership_access_filter", true);
+        register_rest_route( 'richie/v1', '/news', array(
+            'methods' => 'GET',
+            'callback' => array($this, 'feed_route_handler'),
+            ) );
+        }
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Richie_News_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Richie_News_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+        * Register the stylesheets for the public-facing side of the site.
+        *
+        * @since    1.0.0
+        */
+        public function enqueue_styles() {
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/richie-news-public.css', array(), $this->version, 'all' );
+            /**
+            * This function is provided for demonstration purposes only.
+            *
+            * An instance of this class should be passed to the run() function
+            * defined in Richie_News_Loader as all of the hooks are defined
+            * in that particular class.
+            *
+            * The Richie_News_Loader will then create the relationship
+            * between the defined hooks and the functions defined in this
+            * class.
+            */
 
-	}
+            wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/richie-news-public.css', array(), $this->version, 'all' );
 
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+        }
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Richie_News_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Richie_News_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        /**
+        * Register the JavaScript for the public-facing side of the site.
+        *
+        * @since    1.0.0
+        */
+        public function enqueue_scripts() {
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/richie-news-public.js', array( 'jquery' ), $this->version, false );
+            /**
+            * This function is provided for demonstration purposes only.
+            *
+            * An instance of this class should be passed to the run() function
+            * defined in Richie_News_Loader as all of the hooks are defined
+            * in that particular class.
+            *
+            * The Richie_News_Loader will then create the relationship
+            * between the defined hooks and the functions defined in this
+            * class.
+            */
 
-	}
+            wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/richie-news-public.js', array( 'jquery' ), $this->version, false );
 
-}
+        }
+
+    }
 
 
