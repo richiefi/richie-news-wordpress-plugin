bbc2c0fafb6904fb38147d8041f226fcfb44e84f
Markku Uusitupa <markku@richie.fi>
2019-02-26 18:25:04 +0200

    Link issues to signin route (which then redirects to the actual issue).

--- richie-news/public/partials/richie-maggio-index-display.php
+++ richie-news/public/partials/richie-maggio-index-display.php
@@ -17,8 +17,10 @@
 <div class="maggio-container" style="display:flex;">
     <?php foreach( $issues as $issue ): ?>
         <div class="maggio-issue">
-            <img src="<?php echo $issue->get_cover(200,300); ?>">
-            <?php echo $issue->title ?>
+            <a href="<?php echo $issue->get_redirect_url(); ?>">
+                <img src="<?php echo $issue->get_cover(200,300); ?>">
+                <?php echo $issue->title ?>
+            </a>
         </div>
     <?php endforeach; ?>
 </div>
