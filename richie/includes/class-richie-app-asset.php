daed715b6b4a2a8d7b5e17c1c86080a1abaabd62
Markku Uusitupa <markku@richie.fi>
2019-03-15 13:01:56 +0200

    Allow configurable prefix for assset object.

--- richie/includes/class-richie-app-asset.php
+++ richie/includes/class-richie-app-asset.php
@@ -4,12 +4,19 @@ class Richie_App_Asset {
     public $local_name;
     public $remote_url;
 
-    function __construct($dependency) {
+    function __construct($dependency, $local_prefix = 'app-assets/') {
         $remote_url = $dependency->src;
         if ( substr( $remote_url, 0, 4 ) !== "http" ) {
             $remote_url = get_site_url(null, $remote_url);
         }
-        $this->local_name = 'app-assets/' . ltrim(wp_make_link_relative($remote_url), '/');
+        $this->local_name = $local_prefix . ltrim(wp_make_link_relative($remote_url), '/');
         $this->remote_url = add_query_arg( 'ver', $dependency->ver, $remote_url );
+    }
+
+    public function __toString() {
+        return json_encode(array(
+            'local_name' => $this->local_name,
+            'remote_url' => $this->remote_url
+        ));
     }
 }
