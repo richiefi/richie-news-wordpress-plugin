6c0f7e78987cbde560e345dba2e26da4e117cfe2
Markku Uusitupa <markku@richie.fi>
2019-04-30 11:18:15 +0300

    Test case for redirection with extra query params.

--- richie/tests/test-maggio-redirect.php
+++ richie/tests/test-maggio-redirect.php
@@ -63,4 +63,48 @@ class MaggioRedirectTest extends WP_UnitTestCase {
         $richie_public->maggio_redirect_request( $wp_mock );
 
     }
+
+    public function test_redirection_with_params() {
+
+        $maggio_service = $this->getMockBuilder( Richie_Maggio_Service::class )
+        ->setConstructorArgs( [ $this->hostname ] )
+        ->setMethods( [ 'is_issue_free' ] )
+        ->getMock();
+
+        // fake free issue
+        $maggio_service->method( 'is_issue_free' )
+        ->willReturn( true );
+
+        $richie_public = $this->getMockBuilder( Richie_Public::class )
+        ->setConstructorArgs( array( 'richie', 'version' ) )
+        ->setMethods( [ 'do_redirect', 'get_maggio_service' ] )
+        ->getMock();
+
+        $richie_public->method( 'get_maggio_service' )
+        ->willReturn( $maggio_service );
+
+        $richie_public->expects( $this->once() )
+            ->method( 'do_redirect' )
+            ->with(
+                $this->logicalAnd(
+                    $this->stringContains( 'https://test.hostname/_signin/' ),
+                    $this->stringContains( '&page=1' ),
+                    $this->stringContains( '&q=search%20term' ),
+                    $this->logicalNot( $this->stringContains( 'should_not_be_included' ) )
+                )
+            );
+
+        $uuid    = wp_generate_uuid4();
+        $wp_mock = new StdClass();
+
+        $wp_mock->query_vars = array(
+            'maggio_redirect' => $uuid,
+            'page'            => 1,
+            'search'          => 'search term',
+            'unknown'         => 'should_not_be_included',
+        );
+
+        $richie_public->maggio_redirect_request( $wp_mock );
+
+    }
 }
