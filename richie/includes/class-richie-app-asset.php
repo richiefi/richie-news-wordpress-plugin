c31a6188b048230b0bb8135ea1b44c74a5ef1605
Markku Uusitupa <markku@richie.fi>
2019-08-19 18:23:33 +0300

    Replace asset local path and path component in remote url with normalized versions.

--- richie/includes/class-richie-app-asset.php
+++ richie/includes/class-richie-app-asset.php
@@ -9,7 +9,12 @@ class Richie_App_Asset {
         if ( substr( $remote_url, 0, 4 ) !== "http" ) {
             $remote_url = get_site_url(null, $remote_url);
         }
-        $this->local_name = $local_prefix . ltrim(wp_make_link_relative($remote_url), '/');
+
+        // Replace path component with normalized path.
+        $remote_path = wp_parse_url( $remote_url, PHP_URL_PATH );
+        $remote_url = str_replace( $remote_path, richie_normalize_path( $remote_path ), $remote_url );
+
+        $this->local_name = richie_normalize_path($local_prefix . ltrim(wp_make_link_relative($remote_url), '/'));
         $this->remote_url = add_query_arg( 'ver', $dependency->ver, $remote_url );
     }
 
