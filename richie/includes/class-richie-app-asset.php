03c77b7671e6a266101c45f451e8b23343813621
Markku Uusitupa <markku@richie.fi>
2020-03-04 16:19:19 +0200

    Parse url and handle protocol relative urls (include scheme using wordpress current scheme).

--- richie/includes/class-richie-app-asset.php
+++ richie/includes/class-richie-app-asset.php
@@ -6,8 +6,18 @@ class Richie_App_Asset {
 
     function __construct($dependency, $local_prefix = 'app-assets/') {
         $remote_url = $dependency->src;
-        if ( substr( $remote_url, 0, 4 ) !== "http" ) {
-            $remote_url = get_site_url(null, $remote_url);
+
+        // this should handle also protocol relative urls
+        $parsed_url = wp_parse_url( $remote_url );
+
+        if ( $parsed_url !== false) {
+            if ( empty( $parsed_url['scheme'] ) ) {
+                if ( isset( $parsed_url['host'] ) ) {
+                    $remote_url = set_url_scheme( $remote_url );
+                } else {
+                    $remote_url = get_site_url( null, $remote_url );
+                }
+            }
         }
 
         $this->local_name = richie_normalize_path($local_prefix . ltrim(wp_make_link_relative($remote_url), '/'));
