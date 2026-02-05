6322555ef0ad5ad64da28ef6a29255c9ec2d853c
Markku Uusitupa <markku@richie.fi>
2026-02-05 10:43:19 +0200

    Tweak preview layout

--- richie/admin/feed-editor/build/index.asset.php
+++ richie/admin/feed-editor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e2f171e8179d181d1a01');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cad0fe676dfe0f363ef3');
